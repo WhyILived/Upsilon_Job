f0b8b909af5ff4af597614c64a665d7ba30ba097
Muhammad Ahmed <melkhedr8@gmail.com>
2025-07-24 08:01:46 -0400

    Cleaned up some issues within app.py of unnecessarily creating a client, created a vellum handler with a crude api to communicate with the vellum workflow TODO: make the api more standard, extend the vellum handler for other tasks, and start integrating the embeddor

---
+++ backend/vellum_handler.py
@@ -0,0 +1,26 @@
+import os
+from vellum.client import Vellum
+import vellum.types as types
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+
+project_root = Path(__file__).parent.parent
+load_dotenv(project_root / '.env')
+# create your API key here: https://app.vellum.ai/api-keys#keys
+client = Vellum(
+  api_key=os.environ.get("VELLUM_API_KEY")
+)
+def parse_resume(resume_JSON : dict):    
+    result = client.execute_workflow(
+        workflow_deployment_name="bullet-point-terms",
+        release_tag="LATEST",
+        inputs=[
+            types.WorkflowRequestJsonInputRequest(
+                name="BuildingBlocks",
+                type="JSON",
+                value=resume_JSON,
+            ),
+        ],
+    )
+    return result.model_dump()['data']['outputs'][0]['value']
